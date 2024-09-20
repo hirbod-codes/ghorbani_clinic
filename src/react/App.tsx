49827872f9e2724a26704f329f3a4d6c46c97177
Hirbod Khatami <hirbod.khatami.words@gmail.com>
2024-09-20 14:14:00 +0000

    feat: page slide animation

--- src/react/App.tsx
+++ src/react/App.tsx
@@ -1,7 +1,7 @@
-import { useContext, useEffect, useRef, useState } from 'react'
+import { useContext, useState } from 'react'
 
 import { HomeOutlined, PersonOutlined, SettingsOutlined, MenuOutlined, LogoutOutlined, LightModeOutlined, DarkModeOutlined, ExpandLess, ExpandMore, DisplaySettingsOutlined, StorageOutlined, MasksOutlined, AccessTimeOutlined, LoginOutlined, FormatPaintOutlined, DeleteOutline, RepeatOutlined, } from '@mui/icons-material'
-import { AppBar, Drawer, IconButton, List, ListItemButton, ListItemIcon, ListItemText, Toolbar, Typography, Collapse, CircularProgress, Grid, Theme, colors, Dialog, DialogTitle, DialogActions, Button, Stack, Slide, Box } from '@mui/material'
+import { AppBar, Drawer, IconButton, List, ListItemButton, ListItemIcon, ListItemText, Toolbar, Typography, Collapse, CircularProgress, Theme, colors, Dialog, DialogTitle, DialogActions, Button, Stack, Box } from '@mui/material'
 
 import { AuthContext } from './Contexts/AuthContext'
 import { resources } from '../Electron/Database/Repositories/Auth/resources'
@@ -13,7 +13,7 @@ import { DbSettings } from './Pages/Settings/DbSettings'
 import { t } from 'i18next'
 import { General } from './Pages/Settings/General'
 import { MenuBar } from './Components/MenuBar'
-import { ThemeContext, jsx } from '@emotion/react'
+import { ThemeContext } from '@emotion/react'
 import { ConfigurationContext } from './Contexts/ConfigurationContext'
 import { getReactLocale } from './Lib/helpers'
 import { NavigationContext } from './Contexts/NavigationContext'
@@ -21,7 +21,7 @@ import { ThemeSettings } from './Pages/Settings/ThemeSettings'
 import { dbAPI } from '../Electron/Database/dbAPI'
 import { publish } from './Lib/Events'
 import { RESULT_EVENT_NAME } from './Contexts/ResultWrapper'
-import { animated, config, useSpring } from 'react-spring'
+import { PageSlider } from './PageSlider'
 
 export function App() {
     const nav = useContext(NavigationContext)
@@ -257,86 +257,9 @@ export function App() {
                 </AppBar>
 
                 <Box sx={{ overflow: 'hidden', height: '100%', width: '100%' }}>
-                    <Page page={nav?.content} />
+                    <PageSlider page={nav?.content} />
                 </Box>
             </Stack>
         </>
-    )
-}
-
-export function Page({ page }: { page: JSX.Element }) {
-    const [page1, setPage1] = useState<JSX.Element>()
-    const [page2, setPage2] = useState<JSX.Element>()
-
-    // Animations
-    const pageAnimationConfig1 = { ...config.default, precision: 0.0001 }
-    const [pageAnimation1, pageAnimation1Api] = useSpring(() => ({ from: { left: '0' }, config: pageAnimationConfig1 }));
-
-    const pageAnimationConfig2 = { ...config.default, precision: 0.0001 }
-    const [pageAnimation2, pageAnimation2Api] = useSpring(() => ({ from: { left: '0' }, config: pageAnimationConfig2 }));
-
-    console.log('Page', { page1, page2 })
-
-    useEffect(() => {
-        console.group('Page', 'useEffect')
-
-        console.log({ page1, page2 })
-
-        try {
-            if (pageAnimation2.left.get() === '100%') {
-                pageAnimation2Api.set({ left: '-100%' })
-                setPage2(page)
-
-                console.log({ page1, page2 })
-
-                pageAnimation1Api.start({
-                    from: { left: '0' },
-                    to: { left: '100%' },
-                    config: pageAnimationConfig1
-                })
-
-                setTimeout(async () => {
-                    pageAnimation2Api.start({
-                        from: { left: '-100%' },
-                        to: { left: '0' },
-                        config: pageAnimationConfig2
-                    })
-                }, 200)
-            }
-            else {
-                pageAnimation1Api.set({ left: '-100%' })
-                setPage1(page)
-
-                console.log({ page1, page2 })
-
-                pageAnimation2Api.start({
-                    from: { left: '0' },
-                    to: { left: '100%' },
-                    config: pageAnimationConfig2
-                })
-
-                setTimeout(async () => {
-                    pageAnimation1Api.start({
-                        from: { left: '-100%' },
-                        to: { left: '0' },
-                        config: pageAnimationConfig1
-                    })
-                }, 200)
-            }
-        } finally {
-            console.groupEnd()
-        }
-    }, [page])
-
-    return (
-        <div style={{ overflow: 'hidden', position: 'relative', height: '100%' }}>
-            <animated.div style={{ overflow: 'hidden', height: '100%', width: '100%', position: 'absolute', ...pageAnimation1 }}>
-                {page1}
-            </animated.div>
-
-            <animated.div style={{ overflow: 'hidden', height: '100%', width: '100%', position: 'absolute', ...pageAnimation2 }}>
-                {page2}
-            </animated.div>
-        </div>
     )
 }
