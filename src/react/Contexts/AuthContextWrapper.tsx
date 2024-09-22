33a02d8e105e24d479c5770c7a6cb95b6cc7742a
Hirbod Khatami <hirbod.khatami.words@gmail.com>
2024-09-22 10:37:31 +0000

    Update

--- src/react/Contexts/AuthContextWrapper.tsx
+++ src/react/Contexts/AuthContextWrapper.tsx
@@ -119,10 +119,6 @@ export function AuthContextWrapper({ children }: { children?: ReactNode; }) {
         try {
             console.group('AuthContextWrapper', 'init')
 
-            console.log('should end?', hasInitialized.current || isAuthLoading.current || !configuration?.hasFetchedConfig || configuration?.showDbConfigurationModal || auth.user || auth.ac)
-            if (hasInitialized.current || isAuthLoading.current || !configuration?.hasFetchedConfig || configuration?.showDbConfigurationModal || auth.user || auth.ac)
-                return
-
             isAuthLoading.current = true
             hasInitialized.current = true
 
@@ -155,7 +151,9 @@ export function AuthContextWrapper({ children }: { children?: ReactNode; }) {
     }
 
     useEffect(() => {
-        init()
+        console.log('AuthContextWrapper', 'useEffect', 'should init?', !hasInitialized.current && !isAuthLoading.current && configuration?.hasFetchedConfig && !configuration?.showDbConfigurationModal && (!auth.user || !auth.ac))
+        if (!hasInitialized.current && !isAuthLoading.current && configuration?.hasFetchedConfig && !configuration?.showDbConfigurationModal && (!auth.user || !auth.ac))
+            init()
     }, [])
 
     return (
--- src/react/PageSlider.tsx
+++ src/react/PageSlider.tsx
@@ -1,3 +1,4 @@
+import { useTheme } from '@mui/material';
 import { AnimatePresence, motion } from 'framer-motion';
 
 const xOffset = 100;
@@ -9,18 +10,21 @@ const variants = {
     active: {
         left: 0,
         opacity: 1,
-        transition: { delay: 0.2 }
+        transition: { delay: 0.8, ease: 'easeIn' }
     },
     exit: {
         left: xOffset,
         opacity: 0,
+        transition: { delay: 0, ease: 'easeIn' }
     }
 };
 
 export function PageSlider({ page }: { page: JSX.Element; }) {
+    const theme = useTheme()
+
     return (
         <div style={{ overflow: 'hidden', position: 'relative', height: '100%' }}>
-            <AnimatePresence>
+            <AnimatePresence mode='sync' initial={false}>
                 <motion.div
                     layout
                     key={page.type.name}
@@ -32,6 +36,15 @@ export function PageSlider({ page }: { page: JSX.Element; }) {
                 >
                     {page}
                 </motion.div>
+                <motion.div
+                    layout
+                    key={page.type.name + '1'}
+                    style={{ overflow: 'hidden', backgroundColor: theme.palette.primary[theme.palette.mode], height: '100%', left: '-100%', width: '100%', position: 'absolute' }}
+                    initial={false}
+                    animate={false}
+                    exit={{ left: '500%', scaleX: 5 }}
+                    transition={{ delay: 0.2, duration: 1, ease: 'easeIn' }}
+                />
             </AnimatePresence>
         </div>
     );
