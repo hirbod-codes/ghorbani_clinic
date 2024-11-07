0f992b4be588a787b3474ce17574138df6d27c17
Hirbod Khatami <hirbod.khatami.words@gmail.com>
2024-11-07 22:41:33 +0000

    Update

--- src/react/Components/MedicalHistory/MedicalHistorySearch.tsx
+++ src/react/Components/MedicalHistory/MedicalHistorySearch.tsx
@@ -1,35 +1,54 @@
-import { Done, DoneOutline, RemoveOutlined, SearchOutlined } from "@mui/icons-material";
-import { Box, CircularProgress, IconButton, InputAdornment, List, ListItem, ListItemIcon, ListItemText, Paper, Stack, TextField } from "@mui/material";
+import { Done, SearchOutlined } from "@mui/icons-material";
+import { CircularProgress, IconButton, InputAdornment, List, ListItem, ListItemIcon, ListItemText, Paper, Stack, TextField, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Button } from "@mui/material";
 import { AnimatePresence, motion } from "framer-motion";
 import { t } from "i18next";
-import { useState } from "react";
+import { useContext, useMemo, useState } from "react";
 import { MedicalHistory } from "../../../Electron/Database/Models/MedicalHistory";
 import { mainTransition } from "../../Styles/animations";
-import { RendererDbAPI } from "src/Electron/Database/renderer";
-import { publish } from "src/react/Lib/Events";
-import { RESULT_EVENT_NAME } from "src/react/Contexts/ResultWrapper";
+import { RendererDbAPI } from "../../../Electron/Database/renderer";
 import { TrashIcon } from "../Icons/TrashIcon";
+import { AuthContext } from "../../Contexts/AuthContext";
+import { resources } from "../../../Electron/Database/Repositories/Auth/resources";
+import { publish } from "../../Lib/Events";
+import { RESULT_EVENT_NAME } from "../../Contexts/ResultWrapper";
 
 const xOffset = 100;
+const delay = 100
 const variants = {
-    enter: {
+    enter: (i: number) => ({
         name: 'enter',
         x: -xOffset.toString() + '%',
-        transition: mainTransition
-    },
+        transition: { ...mainTransition, delay: i * delay }
+    }),
     active: {
         name: 'active',
         x: 0,
         transition: { ...mainTransition, delay: 0.5 }
     },
-    exit: {
+    exit: (i: number) => ({
         name: 'exit',
         x: xOffset.toString() + '%',
-        transition: mainTransition
-    }
+        transition: { ...mainTransition, delay: i * delay }
+    })
 };
 
 export function MedicalHistorySearch() {
+    const auth = useContext(AuthContext)
+
+    const initDialog: any = {
+        open: false,
+        title: '',
+        content: '',
+        action: null
+    }
+    const [dialog, setDialog] = useState<{
+        open: boolean,
+        title?: string,
+        content: string,
+        action: () => void | Promise<void>
+    }>(initDialog)
+    const closeDialog = () => setDialog(initDialog)
+
     const [searchStr, setSearchStr] = useState<string>('')
     const [medicalHistories, setMedicalHistories] = useState<MedicalHistory[]>([])
 
@@ -44,12 +63,40 @@ export function MedicalHistorySearch() {
         if (res.code !== 200 || !res.data)
             return false
 
-        setMedicalHistories(res.data)
-
         setIsSearching(false)
+
+        setMedicalHistories(res.data)
 
         return true
     }
+
+    const deletesMedicalHistory = useMemo(() => auth.user && auth.accessControl && auth.accessControl.can(auth.user.roleName).delete(resources.MEDICAL_HISTORY), [auth])
+
+    const deleteMedicalHistory = async (id: string) =>
+        setDialog({
+            open: true,
+            content: t('MedicalHistories.deleteMedicalHistoryDialogContent'),
+            action: async () => {
+                try {
+                    console.group('MedicalHistories', 'deletesMedicalHistory', 'onClick', id)
+
+                    const res = await (window as typeof window & { dbAPI: RendererDbAPI }).dbAPI.deleteMedicalHistoryById(id)
+                    console.log({ res })
+
+                    if (res.code !== 200 || !res.data.acknowledged || res.data.deletedCount !== 1) {
+                        publish(RESULT_EVENT_NAME, {
+                            severity: 'error',
+                            message: t('MedicalHistories.failedToDeleteMedicalHistory')
+                        })
+                        return
+                    }
+
+                    await search()
+                }
+                catch (err) { console.error(err) }
+                finally { console.groupEnd() }
+            }
+        })
 
     return (
         <>
@@ -77,7 +124,7 @@ export function MedicalHistorySearch() {
                 </Stack>
 
                 <Paper sx={{ flexGrow: 2, width: '100%', overflow: 'hidden' }} elevation={3}>
-                    <AnimatePresence>
+                    <AnimatePresence mode='wait'>
                         <List>
                             {
                                 medicalHistories.map((md, i) =>
@@ -86,15 +133,18 @@ export function MedicalHistorySearch() {
                                         initial='enter'
                                         animate='active'
                                         exit='exit'
-                                        variants={variants}
+                                        variants={{ ...variants }}
                                         transition={mainTransition}
                                         style={{ width: '100%' }}
                                     >
                                         <ListItem>
                                             <ListItemText primary={md.name} />
-                                            <ListItemIcon>
-                                                <TrashIcon color="red" />
-                                            </ListItemIcon>
+
+                                            {deletesMedicalHistory &&
+                                                <IconButton onClick={() => deleteMedicalHistory(md._id as string)}>
+                                                    <TrashIcon color="red" />
+                                                </IconButton>
+                                            }
                                         </ListItem>
                                     </motion.div>
                                 )
@@ -102,7 +152,30 @@ export function MedicalHistorySearch() {
                         </List>
                     </AnimatePresence>
                 </Paper>
-            </Stack>
+            </Stack >
+
+            <Dialog open={dialog.open} onClose={closeDialog} >
+                {dialog.title &&
+                    <DialogTitle>
+                        {dialog.title}
+                    </DialogTitle>
+                }
+                <DialogContent>
+                    <DialogContentText whiteSpace={'break-spaces'}>
+                        {dialog.content}
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={closeDialog}>{t('MedicalHistories.No')}</Button>
+                    <Button onClick={async () => {
+                        if (dialog.action)
+                            await dialog.action()
+                        closeDialog()
+                    }}>
+                        {t('MedicalHistories.Yes')}
+                    </Button>
+                </DialogActions>
+            </Dialog>
         </>
     )
 }
