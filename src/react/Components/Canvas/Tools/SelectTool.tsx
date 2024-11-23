3915c8b6691a39b4231857e3dad6bb52251508a4
Hirbod Khatami <hirbod.khatami.words@gmail.com>
2024-11-23 07:18:03 +0000

    Update SelectTool.tsx

--- src/react/Components/Canvas/Tools/SelectTool.tsx
+++ src/react/Components/Canvas/Tools/SelectTool.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react"
 import { Shapes } from "../Shapes/Shapes"
 import { Draw, Point, Position } from "../types"
 import { Rectangle } from "../Shapes/Rectangle"
-import { applyToPoint, fromObject } from "transformation-matrix"
+import { applyToPoint, decomposeTSR, fromObject } from "transformation-matrix"
 import { Stack } from "@mui/material"
 import { Shape } from "../Shapes/Shape"
 
@@ -64,81 +64,108 @@ export function SelectTool({ shapes, canvasBackground, setOnDraw, setOnHoverHook
                 shape.rotate(absoluteRad, centerPoint)
             }
         } else {
-            const p1 = applyToPoint(shape.transformArgs, handlersBoundaries.topLeft.topLeft)
-            const p2 = applyToPoint(shape.transformArgs, handlersBoundaries.bottomRight.bottomRight)
-            let xFlipped = false
-            let xRange: [number, number]
-            if (p1.x > p2.x) {
-                xFlipped = true
-                xRange = [p2.x, p1.x]
+            function calcRectWidth(shape: Rectangle, pointer: Point, side: string) {
+                if (!side.toLowerCase().includes('left') && !side.toLowerCase().includes('right'))
+                    return
+
+                const topLeft = applyToPoint(fromObject(shape.transformArgs), handlersBoundaries.topLeft.topLeft)
+                const topRight = applyToPoint(fromObject(shape.transformArgs), handlersBoundaries.topRight.topRight)
+                const bottomRight = applyToPoint(fromObject(shape.transformArgs), handlersBoundaries.bottomRight.bottomRight)
+                const bottomLeft = applyToPoint(fromObject(shape.transformArgs), handlersBoundaries.bottomLeft.bottomLeft)
+                const [p1, p2] = side.toLowerCase().includes('left') ? [topLeft, bottomLeft] : [bottomRight, topRight]
+
+                const distance = pointFromLineDistance(p1, p2, draw.currentPoint)
+                if (Number.isNaN(distance) || distance === Infinity)
+                    return
+
+                let shouldAdd: boolean
+
+                let y = lineFunction(p1, p2, pointer.x)
+
+                if (y === Infinity)
+                    return
+
+                if (y === undefined && pointer.x < p1.x)
+                    shouldAdd = false
+                if (y === undefined && pointer.x >= p1.x)
+                    shouldAdd = true
+                if (y !== undefined && pointer.y <= y)
+                    shouldAdd = false
+                if (y !== undefined && pointer.y > y)
+                    shouldAdd = true
+
+                if (!side.toLowerCase().includes('right'))
+                    shouldAdd = !shouldAdd
+
+                const decomposedMatrix = decomposeTSR(fromObject(shape.transformArgs))
+                if ((decomposedMatrix.rotation.angle * 180 / Math.PI) < 0)
+                    shouldAdd = !shouldAdd
+
+                if (side.toLowerCase().includes('left'))
+                    if (shouldAdd)
+                        addLeft(shape, distance)
+                    else
+                        minusLeft(shape, distance)
+
+                if (side.toLowerCase().includes('right'))
+                    if (shouldAdd)
+                        addRight(shape, distance)
+                    else
+                        minusRight(shape, distance)
+
+                return
             }
-            else
-                xRange = [p1.x, p2.x]
 
-            let yFlipped = false
-            let yRange: [number, number]
-            if (p1.y > p2.y) {
-                yFlipped = true
-                yRange = [p2.y, p1.y]
-            }
-            else
-                yRange = [p1.y, p2.y]
+            function calcRectHeight(shape: Rectangle, pointer: Point, side: string) {
+                if (!side.toLowerCase().includes('top') && !side.toLowerCase().includes('bottom'))
+                    return
 
-            const handlerRefPoint = applyToPoint(fromObject(shape.transformArgs), handlersBoundaries[selectedHandler][selectedHandler])
+                const topLeft = applyToPoint(fromObject(shape.transformArgs), handlersBoundaries.topLeft.topLeft)
+                const topRight = applyToPoint(fromObject(shape.transformArgs), handlersBoundaries.topRight.topRight)
+                const bottomRight = applyToPoint(fromObject(shape.transformArgs), handlersBoundaries.bottomRight.bottomRight)
+                const bottomLeft = applyToPoint(fromObject(shape.transformArgs), handlersBoundaries.bottomLeft.bottomLeft)
+                const [p1, p2] = side.toLowerCase().includes('bottom') ? [bottomRight, bottomLeft] : [topLeft, topRight]
 
-            const verticalDiff = Math.abs(draw.currentPoint.y - handlerRefPoint.y) - 5
-            const horizontalDiff = Math.abs(draw.currentPoint.x - handlerRefPoint.x) - 5
+                const distance = pointFromLineDistance(p1, p2, draw.currentPoint)
+                if (Number.isNaN(distance) || distance === Infinity)
+                    return
 
-            function calcRectWidth(shape: Rectangle, point: Point, range: [number, number], side: string) {
-                if (side.toLowerCase().includes('left')) {
-                    if (!xFlipped && point.x > range[1])
-                        return
-                    if (xFlipped && point.x < range[0])
-                        return
-                } else {
-                    if (!xFlipped && point.x < range[0])
-                        return
-                    if (xFlipped && point.x > range[1])
-                        return
-                }
+                let shouldAdd: boolean
 
-                if (point.x <= range[1] && point.x >= range[0]) {
-                    if (side.toLowerCase().includes('left'))
-                        minusLeft(shape, horizontalDiff)
+                let y = lineFunction(p1, p2, pointer.x)
+
+                if (y === Infinity)
+                    return
+
+                if (y === undefined && pointer.x < p1.x)
+                    shouldAdd = false
+                if (y === undefined && pointer.x >= p1.x)
+                    shouldAdd = true
+                if (y !== undefined && pointer.y <= y)
+                    shouldAdd = false
+                if (y !== undefined && pointer.y > y)
+                    shouldAdd = true
+
+                const decomposedMatrix = decomposeTSR(fromObject(shape.transformArgs))
+                if ((decomposedMatrix.rotation.angle * 180 / Math.PI) < 0)
+                    shouldAdd = !shouldAdd
+
+                if (!side.toLowerCase().includes('bottom'))
+                    shouldAdd = !shouldAdd
+
+                if (side.toLowerCase().includes('top'))
+                    if (shouldAdd)
+                        addTop(shape, distance)
                     else
-                        minusRight(shape, horizontalDiff)
-                } else {
-                    if (side.toLowerCase().includes('left'))
-                        addLeft(shape, horizontalDiff)
-                    else
-                        addRight(shape, horizontalDiff)
-                }
-            }
+                        minusTop(shape, distance)
 
-            function calcRectHeight(shape: Rectangle, point: Point, range: [number, number], side: string) {
-                if (side.toLowerCase().includes('top')) {
-                    if (!yFlipped && point.y > range[1])
-                        return
-                    if (yFlipped && point.y < range[0])
-                        return
-                } else {
-                    if (!yFlipped && point.y < range[0])
-                        return
-                    if (yFlipped && point.y > range[1])
-                        return
-                }
+                if (side.toLowerCase().includes('bottom'))
+                    if (shouldAdd)
+                        addBottom(shape, distance)
+                    else
+                        minusBottom(shape, distance)
 
-                if (point.y <= range[1] && point.y >= range[0]) {
-                    if (side.toLowerCase().includes('top'))
-                        minusTop(shape, verticalDiff)
-                    else
-                        minusBottom(shape, verticalDiff)
-                } else {
-                    if (side.toLowerCase().includes('top'))
-                        addTop(shape, verticalDiff)
-                    else
-                        addBottom(shape, verticalDiff)
-                }
+                return
             }
 
             function addRight(shape: Rectangle, x: number) {
@@ -192,59 +219,10 @@ export function SelectTool({ shapes, canvasBackground, setOnDraw, setOnHoverHook
                 shape.h -= y
             }
 
-            if (verticalDiff > 0 || horizontalDiff > 0)
-                switch (selectedHandler) {
-                    case 'topLeft':
-                        if (shape instanceof Rectangle)
-                            calcRectWidth(shape, draw.currentPoint, xRange, selectedHandler)
-                        if (shape instanceof Rectangle)
-                            calcRectHeight(shape, draw.currentPoint, yRange, selectedHandler)
-                        break;
-
-                    case 'top':
-                        if (shape instanceof Rectangle)
-                            calcRectHeight(shape, draw.currentPoint, yRange, selectedHandler)
-                        break;
-
-                    case 'topRight':
-                        if (shape instanceof Rectangle)
-                            calcRectWidth(shape, draw.currentPoint, xRange, selectedHandler)
-                        if (shape instanceof Rectangle)
-                            calcRectHeight(shape, draw.currentPoint, yRange, selectedHandler)
-                        break;
-
-                    case 'right':
-                        if (shape instanceof Rectangle)
-                            calcRectWidth(shape, draw.currentPoint, xRange, selectedHandler)
-                        break;
-
-                    case 'bottomRight':
-                        if (shape instanceof Rectangle)
-                            calcRectWidth(shape, draw.currentPoint, xRange, selectedHandler)
-                        if (shape instanceof Rectangle)
-                            calcRectHeight(shape, draw.currentPoint, yRange, selectedHandler)
-                        break;
-
-                    case 'bottom':
-                        if (shape instanceof Rectangle)
-                            calcRectHeight(shape, draw.currentPoint, yRange, selectedHandler)
-                        break;
-
-                    case 'bottomLeft':
-                        if (shape instanceof Rectangle)
-                            calcRectWidth(shape, draw.currentPoint, xRange, selectedHandler)
-                        if (shape instanceof Rectangle)
-                            calcRectHeight(shape, draw.currentPoint, yRange, selectedHandler)
-                        break;
-
-                    case 'left':
-                        if (shape instanceof Rectangle)
-                            calcRectWidth(shape, draw.currentPoint, xRange, selectedHandler)
-                        break;
-
-                    default:
-                        break;
-                }
+            if (shape instanceof Rectangle) {
+                calcRectWidth(shape, draw.currentPoint, selectedHandler)
+                calcRectHeight(shape, draw.currentPoint, selectedHandler)
+            }
         }
 
         shapes.draw(draw)
@@ -349,6 +327,23 @@ export function SelectTool({ shapes, canvasBackground, setOnDraw, setOnHoverHook
     }
 
     const twoPointDistance = (p1: Point, p2: Point): number => Math.sqrt(Math.pow((p1.x - p2.x), 2) + Math.pow((p1.y - p2.y), 2))
+
+    const pointFromLineDistance = (p1: Point, p2: Point, p: Point): number => {
+        console.log({ p1, p2, p })
+        // area = (1/2) |x1(y2 - y3) + x2(y3 - y1) + x3(y1 - y2)| = (1/2) p1.p2 * p
+        return Math.abs(p.x * (p1.y - p2.y) + p1.x * (p2.y - p.y) + p2.x * (p.y - p1.y)) / twoPointDistance(p1, p2)
+    }
+
+    const lineFunction = (p1: Point, p2: Point, x: number): number => {
+        const m = (p2.y - p1.y) / (p2.x - p1.x)
+        if (m === Infinity)
+            return p1.x === x ? Infinity : undefined
+        if (m === 0)
+            return p1.y
+
+        const c = p2.y - p2.x * m
+        return m * x + c
+    }
 
     useEffect(() => {
         setOnDraw(() => onMoveHook)
