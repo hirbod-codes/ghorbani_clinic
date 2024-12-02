4af95e6d68dec53b99ad4cb695df9d67e366d8e7
Hirbod Khatami <hirbod.khatami.words@gmail.com>
2024-12-02 23:16:24 +0000

    Create appRendererEvents.ts

---
+++ src/Electron/appRendererEvents.ts
@@ -0,0 +1,21 @@
+import { ipcRenderer, OpenDialogReturnValue, SaveDialogReturnValue } from "electron"
+
+export type appAPI = {
+    reLaunch: () => void;
+    saveFile: ({ content, path }: { content: string, path: string }) => Promise<boolean>;
+    saveFileDialog: (options?: { defaultPath?: string, message?: string, buttonLabel?: string, title?: string }) => Promise<SaveDialogReturnValue>;
+    openDirectoryDialog: (options?: { defaultPath?: string, message?: string, buttonLabel?: string, title?: string }) => Promise<OpenDialogReturnValue>;
+    openFileDialog: (options?: { defaultPath?: string, message?: string, buttonLabel?: string, title?: string }) => Promise<OpenDialogReturnValue>;
+    setAppIcon: () => Promise<boolean>;
+}
+
+export function handleAppRendererEvents(): appAPI {
+    return {
+        reLaunch: () => ipcRenderer.send('relaunch-app'),
+        saveFile: ({ content, path }: { content: string, path: string }): Promise<boolean> => ipcRenderer.invoke('save-file', { content, path }),
+        saveFileDialog: (options?: { defaultPath?: string, message?: string, buttonLabel?: string, title?: string }): Promise<SaveDialogReturnValue> => ipcRenderer.invoke('save-file-dialog', options),
+        openDirectoryDialog: (options?: { defaultPath?: string, message?: string, buttonLabel?: string, title?: string }): Promise<OpenDialogReturnValue> => ipcRenderer.invoke('open-directory-dialog', options),
+        openFileDialog: (options?: { defaultPath?: string, message?: string, buttonLabel?: string, title?: string }): Promise<OpenDialogReturnValue> => ipcRenderer.invoke('open-file-dialog', options),
+        setAppIcon: (): Promise<boolean> => ipcRenderer.invoke('set-app-icon'),
+    }
+}
