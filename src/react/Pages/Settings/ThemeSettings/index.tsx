6d5898f1e269cb30cbb1e527b4ab91696945da65
Hirbod Khatami <hirbod.khatami.words@gmail.com>
2025-01-02 18:58:59 +0000

    Update index.tsx

--- src/react/Pages/Settings/ThemeSettings/index.tsx
+++ src/react/Pages/Settings/ThemeSettings/index.tsx
@@ -11,6 +11,8 @@ import { RGB } from "@/src/react/Lib/Colors/RGB";
 import { IColor } from "@/src/react/Lib/Colors/IColor";
 import { HSV } from "@/src/react/Lib/Colors/HSV";
 import { HSL } from "@/src/react/Lib/Colors/HSL";
+import { ColorPicker } from "@/src/react/Components/ColorPicker";
+import { Checkbox } from "@mui/material";
 
 export const ThemeSettings = memo(function ThemeSettings() {
     const c = useContext(ConfigurationContext)!
@@ -53,6 +55,25 @@ export const ThemeSettings = memo(function ThemeSettings() {
 
     return (
         <>
+            <div className="grid grid-cols-12">
+                <div className="grid md:col-span-4">
+                    <div className="size-full flex flex-col items-stretch space-y-4 space-x-2">
+                        <div>
+                            <p className="text-xl">Core Colors</p>
+                            <p className="text-sm ">Override or set key colors that will be used to generate tonal palettes and schemes.</p>
+                        </div>
+                        <div>
+                            <p className="text-xl">Color match</p>
+                            <div className="flex flex-row items-center justify-between w-full">
+                                <p className="text-sm ">Stay true to my color inputs.</p>
+                                <Checkbox defaultChecked={true} size='small' />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="grid">
+                </div>
+            </div>
             <div className="flex flex-row flex-wrap items-start content-start size-full p-3 *:m-1 overflow-y-auto">
                 {/* Color tones */}
                 <ColorTones />
@@ -146,9 +167,65 @@ export const ThemeSettings = memo(function ThemeSettings() {
     )
 })
 
+const calculateTintAndShade = (
+    hexColor, // using #663399 as an example
+    percentage = 0.1 // using 10% as an example
+) => {
+    const r = parseInt(hexColor.slice(1, 3), 16); // r = 102
+    const g = parseInt(hexColor.slice(3, 5), 16); // g = 51
+    const b = parseInt(hexColor.slice(5, 7), 16); // b = 153
+
+    /* 
+       From this part, we are using our two formulas
+       in this case, here is the formula for tint,
+       please be aware that we are performing two validations
+       we are using Math.min to set the max level of tint to 255,
+       so we don't get values like 280 ;)
+       also, we have the Math.round so we don't have values like 243.2
+       both validations apply for both tint and shade as you can see */
+    const tintR = Math.round(Math.min(255, r + (255 - r) * percentage)); // 117
+    const tintG = Math.round(Math.min(255, g + (255 - g) * percentage)); // 71
+    const tintB = Math.round(Math.min(255, b + (255 - b) * percentage)); // 163
+
+
+    const shadeR = Math.round(Math.max(0, r - r * percentage)); // 92
+    const shadeG = Math.round(Math.max(0, g - g * percentage)); // 46
+    const shadeB = Math.round(Math.max(0, b - b * percentage)); // 138
+
+
+    /* 
+       Now with all the values calculated, the only missing stuff is 
+       getting our color back to hexadecimal, to achieve that, we are going
+       to perform a toString(16) on each value, so we get the hex value
+       for each color, and then we just append each value together and voilà!*/
+    return {
+        tint: {
+            r: tintR,
+            g: tintG,
+            b: tintB,
+            hex:
+                '#' +
+                [tintR, tintG, tintB]
+                    .map(x => x.toString(16).padStart(2, '0'))
+                    .join(''), // #7547a3 
+        },
+        shade: {
+            r: shadeR,
+            g: shadeG,
+            b: shadeB,
+            hex:
+                '#' +
+                [shadeR, shadeG, shadeB]
+                    .map(x => x.toString(16).padStart(2, '0'))
+                    .join(''), // #5c2e8a 
+        },
+    };
+};
+
 export function ColorTones() {
     const count = 21
-    const hex = '#ff0000'
+    // const hex = '#000000'
+    const [hex, setHex] = useState('#000000')
     const mid = Math.floor(count / 2)
 
     function getModifiedColor(hex: string, n: number, total: number): IColor {
@@ -165,17 +242,21 @@ export function ColorTones() {
 
     return (
         <>
+            <ColorPicker
+                controlledColor={HSV.fromHex(hex) as HSV}
+                onColorChanging={(c) => setHex(c.toHex())}
+            />
             <div className="w-full overflow-auto m-1 border rounded-lg">
                 <div className="h-14 flex flex-row space-x-1">
                     {Array.from(Array(count).keys()).map((n, i) => {
-                        const c = HSV.fromHex(hex)
+                        let color
                         if (n <= mid)
-                            c.darken(1 - (n / mid))
+                            color = calculateTintAndShade(hex, 1 - (n / mid)).shade.hex
                         else
-                            c.lighten((n - mid) / mid)
+                            color = calculateTintAndShade(hex, -1 + (n / mid)).tint.hex
 
                         return (
-                            <div key={i} className="size-14 flex flex-col justify-center items-center text-gray-500" style={{ backgroundColor: c.toHex() }}>
+                            <div key={i} className="size-14 flex flex-col justify-center items-center text-gray-500" style={{ backgroundColor: color }}>
                                 {n * 5}
                             </div>
                         )
