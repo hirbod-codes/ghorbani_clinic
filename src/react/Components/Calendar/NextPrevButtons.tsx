affe54cf6264e2408bddec54bb6a8f875872db16
Hirbod Khatami <hirbod.khatami.words@gmail.com>
2025-01-10 11:33:28 +0000

    Update NextPrevButtons.tsx

--- src/react/Components/Calendar/NextPrevButtons.tsx
+++ src/react/Components/Calendar/NextPrevButtons.tsx
@@ -1,14 +1,14 @@
 import { ChevronLeftIcon, ChevronRightIcon } from "lucide-react";
 import { memo } from "react";
-import { Button } from "../../shadcn/components/ui/button";
+import { Button } from "../Base/Button";
 
 export const NextPrevButtons = memo(function NextPrevButtons({ onPrev, onNext }: { onPrev: () => void, onNext: () => void }) {
     return (
-        <div className="flex flex-row">
-            <Button isIcon onClick={onPrev}>
+        <div className="flex flex-row space-x-1">
+            <Button isIcon size='sm' variant='outline' onClick={onPrev}>
                 <ChevronLeftIcon strokeWidth={1.5} />
             </Button>
-            <Button isIcon onClick={onNext}>
+            <Button isIcon size='sm' variant='outline' onClick={onNext}>
                 <ChevronRightIcon strokeWidth={1.5} />
             </Button>
         </div>
