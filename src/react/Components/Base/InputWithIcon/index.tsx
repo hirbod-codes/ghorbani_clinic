7ed0ac1ce013bfa0ad1bfd1deac6a9d68324ec07
Hirbod Khatami <hirbod.khatami.words@gmail.com>
2025-02-11 21:54:33 +0000

    Update index.tsx

--- src/react/Components/Base/InputWithIcon/index.tsx
+++ src/react/Components/Base/InputWithIcon/index.tsx
@@ -26,6 +26,7 @@ export function InputWithIcon({ startIcon, startIconProps, endIcon, endIconProps
                     `cursor-pointer flex h-10 w-full rounded-md border border-input bg-surface py-2 px-4 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-0 disabled:cursor-not-allowed disabled:opacity-50${startIcon ? ' pl-8' : ''}${endIcon ? 'pr-8' : ''}`,
                     props?.className
                 )}
+                style={{ direction: local.direction, ...props?.style }}
             />
             {ei && (
                 <div {...eiProps} className={cn("absolute right-3 top-1/2 transform -translate-y-1/2", eiProps?.className)}>
@@ -35,33 +36,4 @@ export function InputWithIcon({ startIcon, startIconProps, endIcon, endIconProps
         </div>
     );
 }
-
-// export function InputWithIcon({ startIcon, startIconProps, endIcon, endIconProps, inputRef, ...props }: { startIcon?: ReactNode, startIconProps?: ComponentProps<'div'>, endIcon?: ReactNode, endIconProps?: ComponentProps<'div'>, inputRef?: RefObject<HTMLInputElement> } & ComponentProps<'input'>) {
-//     const StartIcon = startIcon;
-//     const EndIcon = endIcon;
-
-//     return (
-//         <div className="relative flex items-center cursor-pointer h-10 w-full rounded-md border border-input bg-surface py-2 px-4 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-0 disabled:cursor-not-allowed disabled:opacity-50">
-//             {StartIcon && (
-//                 <div {...startIconProps} className={cn("", startIconProps?.className)}>
-//                     {startIcon}
-//                 </div>
-//             )}
-//             <input
-//                 ref={inputRef}
-//                 {...props}
-//                 type={props?.type ?? 'text'}
-//                 className={cn(
-//                     // `cursor-pointer flex h-10 w-full rounded-md border border-input bg-surface py-2 px-4 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-0 disabled:cursor-not-allowed disabled:opacity-50`,
-//                     props?.className
-//                 )}
-//             />
-//             {EndIcon && (
-//                 <div {...endIconProps} className={cn("", endIconProps?.className)}>
-//                     {endIcon}
-//                 </div>
-//             )}
-//         </div>
-//     );
-// }
 InputWithIcon.displayName = "Input";
