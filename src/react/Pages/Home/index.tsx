9ea020eb898cf752a2b4f7a4a44cde666ce7ccd8
Hirbod Khatami <hirbod.khatami.words@gmail.com>
2025-02-16 10:15:20 +0000

    Update index.tsx

--- src/react/Pages/Home/index.tsx
+++ src/react/Pages/Home/index.tsx
@@ -11,42 +11,40 @@ export const Home = memo(function Home() {
     console.log('Home')
 
     return (
-        <div className="size-full lg:overflow-hidden overflow-auto">
-            <div className="grid gap-2 lg:grid-flow-col grid-cols-12 lg:grid-rows-4 justify-center size-full">
-                <div className="sm:col-span-12 lg:col-span-4 col-span-12 lg:row-span-2">
-                    <Stack direction="vertical" stackProps={{ className: 'h-full' }}>
-                        <Clock containerProps={{ stackProps: { className: 'py-2 border rounded-xl bg-surface-container shadow-sm' } }} />
-                        <Calendar containerProps={{ className: 'flex-grow' }} calendarContainerProps={{ className: 'border rounded-xl bg-surface-container shadow-sm' }} />
-                    </Stack>
-                </div>
+        <div className="grid gap-2 lg:grid-flow-col grid-cols-12 lg:grid-rows-4 justify-center h-full overflow-y-auto  overflow-x-hidden lg:overflow-hidden">
+            <div className="sm:col-span-12 lg:col-span-4 col-span-12 lg:row-span-2">
+                <Stack direction="vertical" stackProps={{ className: 'h-full min-h-[12cm]' }}>
+                    <Clock containerProps={{ stackProps: { className: 'py-2 border rounded-xl bg-surface-container shadow-sm' } }} />
+                    <Calendar containerProps={{ className: 'flex-grow' }} calendarContainerProps={{ className: 'border rounded-xl bg-surface-container shadow-sm' }} />
+                </Stack>
+            </div>
 
-                <div className="sm:col-span-12 lg:col-span-4 col-span-12 lg:row-span-2 p-2 border rounded-xl bg-surface-container shadow-sm">
-                    <Analytics />
-                </div>
+            <div className="sm:col-span-12 lg:col-span-4 col-span-12 lg:row-span-2 p-2 border rounded-xl bg-surface-container shadow-sm">
+                <Analytics />
+            </div>
 
-                {/* <div className="sm:col-span-12 lg:col-span-8 col-span-12 lg:row-span-2 border m-1 rounded-xl bg-surface-container shadow-sm">
-                    <Stack direction="vertical" stackProps={{ className: 'p-4 h-full' }}>
-                        <Stack stackProps={{ className: 'justify-between items-center' }}>
-                            <div className="text-2xl">{t('Home.Patients')}</div>
-                            <div className="text-sm">{t('Home.Monthly')}</div>
-                        </Stack>
-                        <div className="size-full relative flex-grow">
-                            <PatientsChart />
-                        </div>
+            <div className="sm:col-span-12 lg:col-span-8 col-span-12 lg:row-span-2 border rounded-xl bg-surface-container shadow-sm">
+                <Stack direction="vertical" stackProps={{ className: 'p-4 h-full' }}>
+                    <Stack stackProps={{ className: 'justify-between items-center' }}>
+                        <div className="text-2xl">{t('Home.Patients')}</div>
+                        <div className="text-sm">{t('Home.Monthly')}</div>
                     </Stack>
-                </div>
+                    <div className="size-full relative flex-grow">
+                        <PatientsChart />
+                    </div>
+                </Stack>
+            </div>
 
-                <div className="sm:col-span-12 lg:col-span-8 col-span-12 lg:row-span-2 border m-1 rounded-xl bg-surface-container shadow-sm">
-                    <Stack direction="vertical" stackProps={{ className: 'p-4 h-full' }}>
-                        <Stack stackProps={{ className: 'justify-between items-center' }}>
-                            <div className="text-2xl">{t('Home.Visits')}</div>
-                            <div className="text-sm">{t('Home.Daily')}</div>
-                        </Stack>
-                        <div className="size-full relative flex-grow">
-                            <VisitsChart />
-                        </div>
+            <div className="sm:col-span-12 lg:col-span-8 col-span-12 lg:row-span-2 border rounded-xl bg-surface-container shadow-sm">
+                <Stack direction="vertical" stackProps={{ className: 'p-4 h-full' }}>
+                    <Stack stackProps={{ className: 'justify-between items-center' }}>
+                        <div className="text-2xl">{t('Home.Visits')}</div>
+                        <div className="text-sm">{t('Home.Daily')}</div>
                     </Stack>
-                </div> */}
+                    <div className="size-full relative flex-grow">
+                        <VisitsChart />
+                    </div>
+                </Stack>
             </div>
         </div>
     )
