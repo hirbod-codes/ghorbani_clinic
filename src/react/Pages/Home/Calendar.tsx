5a539316d9d9dd1297147a497345b16815ceebf9
Hirbod Khatami <hirbod.khatami.words@gmail.com>
2025-02-17 05:32:01 +0000

    Update Calendar.tsx

--- src/react/Pages/Home/Calendar.tsx
+++ src/react/Pages/Home/Calendar.tsx
@@ -1,4 +1,4 @@
-import { ComponentProps, memo, PointerEvent as ReactPointerEvent, useCallback, useContext, useEffect, useReducer, useRef, useState } from "react";
+import { ComponentProps, memo, PointerEvent as ReactPointerEvent, useCallback, useContext, useEffect, useMemo, useReducer, useRef, useState } from "react";
 import { Visit } from "../../../Electron/Database/Models/Visit";
 import { Calendar as CalendarComponent } from "../../Components/Calendar";
 import { getVisitsInDate, getVisitsPatients } from "../../Components/Visits/helpers";
@@ -12,9 +12,15 @@ import { CircularLoading } from "../../Components/Base/CircularLoading";
 import { Date as DateType } from "../../Lib/DateTime";
 import { Patient } from "@/src/Electron/Database/Models/Patient";
 import { Separator } from "../../shadcn/components/ui/separator";
+import { resources } from "@/src/Electron/Database/Repositories/Auth/resources";
+import { AuthContext } from "../../Contexts/AuthContext";
 
 export const Calendar = memo(function Calendar({ containerProps, calendarContainerProps }: { containerProps?: ComponentProps<'div'>, calendarContainerProps?: ComponentProps<'div'> }) {
+    const auth = useContext(AuthContext)
+
     const configuration = useContext(ConfigurationContext)!
+
+    const lastEventCall = useRef<'onEnter' | 'onLeave' | undefined>(undefined)
 
     const showVisitsStats = useRef<boolean>(false)
     const fetchingVisits = useRef<boolean>(false)
@@ -28,10 +34,6 @@ export const Calendar = memo(function Calendar({ containerProps, calendarContain
     }>({ visits: [], patients: [] })
     const patientsCount = useRef<number>(0)
 
-    // ID of the visit that is taken for its diagnosis representation
-    const [showDiagnosis, setShowDiagnosis] = useState<string | undefined>(undefined)
-    const [showTreatments, setShowTreatments] = useState<string | undefined>(undefined)
-
     const animationFinish = useRef<boolean>(true)
     const [scope, animate] = useAnimate()
 
@@ -39,15 +41,24 @@ export const Calendar = memo(function Calendar({ containerProps, calendarContain
 
     const previousDate = useRef<DateType>()
 
+    const cardRef = useRef<HTMLDivElement>(null)
+    const cardWidth = useRef<number>()
+
     const [, rerender] = useReducer(x => x + 1, 0)
 
-    console.log('Home.Calendar', { showVisitsStats: showVisitsStats.current, isLocked: isLocked.current, visits: visitsPatients, patientsCount, fetchingVisits: fetchingVisits.current, showDiagnosis, showTreatments, containerProps, calendarContainerProps, animationFinish: animationFinish.current, containerRef: containerRef.current })
+    const readsPatient = useMemo(() => auth?.user && auth?.accessControl && auth?.accessControl.can(auth?.user.roleName).read(resources.PATIENT).granted, [auth])
+    const readsVisit = useMemo(() => auth?.user && auth?.accessControl && auth?.accessControl.can(auth?.user.roleName).read(resources.VISIT).granted, [auth])
+
+    // const readsPatient = false
+    // const readsVisit = false
+
+    console.log('Home.Calendar', JSON.stringify({ showVisitsStats: showVisitsStats.current, isLocked: isLocked.current, visitsLength: visitsPatients.current.visits.length, patientsCount, fetchingVisits: fetchingVisits.current, containerProps, calendarContainerProps, animationFinish: animationFinish.current }, undefined, 4))
 
     const updateCard = async (year: number, month: number, day: number) => {
-        if (year === previousDate.current?.year && month === previousDate.current?.month && day === previousDate.current?.day)
-            return
+        // if (year === previousDate.current?.year && month === previousDate.current?.month && day === previousDate.current?.day)
+        //     return
 
-        previousDate.current = { year, month, day }
+        // previousDate.current = { year, month, day }
 
         fetchingVisits.current = true
         const vp = await getVisitsPatients(await getVisitsInDate({ year, month, day }, configuration.local) ?? [])
@@ -61,7 +72,10 @@ export const Calendar = memo(function Calendar({ containerProps, calendarContain
         fetchingVisits.current = false
     }
 
-    const onEnter = async (year: number, month: number, day: number) => {
+    let onEnter: ((year: number, month: number, day: number) => Promise<void>) | undefined = async (year: number, month: number, day: number) => {
+        if (lastEventCall.current === 'onEnter')
+            return
+
         if (isLocked.current)
             return
 
@@ -87,9 +101,14 @@ export const Calendar = memo(function Calendar({ containerProps, calendarContain
 
             rerender()
         }, 100)
+
+        lastEventCall.current = 'onEnter'
     }
 
-    const onLeave = (y: number, m: number, d: number) => {
+    let onLeave: ((y: number, m: number, d: number) => void) | undefined = (y: number, m: number, d: number) => {
+        if (lastEventCall.current === 'onLeave')
+            return
+
         if (isLocked.current)
             return
 
@@ -105,9 +124,11 @@ export const Calendar = memo(function Calendar({ containerProps, calendarContain
 
         showVisitsStats.current = false
         rerender()
+
+        lastEventCall.current = 'onLeave'
     }
 
-    const onDaySelect = async (year, month, day) => {
+    let onDaySelect: ((y: number, m: number, d: number) => Promise<void>) | undefined = async (year, month, day) => {
         // isLocked=false
 
         showVisitsStats.current = true
@@ -118,7 +139,7 @@ export const Calendar = memo(function Calendar({ containerProps, calendarContain
         isLocked.current = true
     }
 
-    const handleClickOnCalendar = useCallback(async function handleClickOnCalendar(e: ReactPointerEvent<HTMLDivElement>) {
+    let handleClickOnCalendar: ((e: ReactPointerEvent<HTMLDivElement>) => Promise<void>) | undefined = useCallback(async function handleClickOnCalendar(e: ReactPointerEvent<HTMLDivElement>) {
         animationFinish.current = false
         // runningAnimations.current.push(0)
         await animate(scope.current, { x: '0', opacity: 0 })
@@ -130,6 +151,13 @@ export const Calendar = memo(function Calendar({ containerProps, calendarContain
         showVisitsStats.current = false
         rerender()
     }, [])
+
+    if (!readsPatient || !readsVisit) {
+        onEnter = undefined
+        onLeave = undefined
+        onDaySelect = undefined
+        handleClickOnCalendar = undefined
+    }
 
     const handleClickOutside = useCallback(async function handleClickOutside(e: PointerEvent) {
         const cRect = containerRef.current?.getBoundingClientRect()
@@ -155,12 +183,12 @@ export const Calendar = memo(function Calendar({ containerProps, calendarContain
     }, [containerRef.current, scope.current])
 
     const checkState = async () => {
-        if (!containerRef.current || animationFinish.current)
-            return
+        // if ( animationFinish.current)
+        //     return
 
         if (document.body.offsetWidth >= 1024) {
             if (isLocked.current || showVisitsStats.current) {
-                let w = containerRef.current.getBoundingClientRect()!.width
+                let w = containerRef.current!.getBoundingClientRect()!.width
                 if (configuration.local.direction === 'rtl')
                     w *= -1
                 await animate(scope.current, { x: w, opacity: 1 })
@@ -173,7 +201,7 @@ export const Calendar = memo(function Calendar({ containerProps, calendarContain
             }
         } else {
             if (isLocked.current || showVisitsStats.current) {
-                let h = containerRef.current.getBoundingClientRect()!.height
+                let h = containerRef.current!.getBoundingClientRect()!.height
                 await animate(scope.current, { y: h, opacity: 1 })
                 animationFinish.current = true
                 rerender()
@@ -186,12 +214,14 @@ export const Calendar = memo(function Calendar({ containerProps, calendarContain
     }
 
     useEffect(() => {
-        console.log('useEffect1', { containerRef: containerRef.current, scope: scope.current })
-        document.body.addEventListener("pointerdown", handleClickOutside);
+        if (readsPatient && readsVisit) {
+            console.log('useEffect1', { containerRef: containerRef.current, scope: scope.current })
+            document.body.addEventListener("pointerdown", handleClickOutside);
 
-        return () => {
-            document.body.removeEventListener("pointerdown", handleClickOutside);
-        };
+            return () => {
+                document.body.removeEventListener("pointerdown", handleClickOutside);
+            };
+        }
     }, [containerRef.current, scope.current]);
 
     useEffect(() => {
@@ -199,7 +229,7 @@ export const Calendar = memo(function Calendar({ containerProps, calendarContain
 
         rerender()
         checkState()
-    }, [isLocked.current, showVisitsStats.current, containerRef?.current])
+    }, [isLocked.current, showVisitsStats.current, containerRef?.current, scope?.current])
 
     useEffect(() => {
         console.log('useEffect3', { containerRef: containerRef.current, scope: scope.current })
@@ -211,9 +241,6 @@ export const Calendar = memo(function Calendar({ containerProps, calendarContain
                 scope.current.style.width = `100%`
         }
     }, [containerRef.current, scope.current]);
-
-    const cardRef = useRef<HTMLDivElement>(null)
-    const cardWidth = useRef<number>()
 
     useEffect(() => {
         console.log('useEffect4', { cardRef: cardRef.current })
@@ -231,30 +258,32 @@ export const Calendar = memo(function Calendar({ containerProps, calendarContain
                         <div className="bg-surface-container-high size-full rounded-lg shadow-lg border overflow-auto p-4">
                             {fetchingVisits.current && <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'><CircularLoading size='lg' /></div>}
 
-                            <Stack direction='vertical' stackProps={{ className: `h-full ${animationFinish.current && !fetchingVisits.current && showVisitsStats.current ? '' : 'hidden'}` }}>
-                                <Stack stackProps={{ className: "items-center justify-between px-2" }}>
-                                    <p>{t('Calendar.PatientsCount')}:</p>
-                                    <p>{patientsCount.current}</p>
-                                </Stack>
-                                <Stack stackProps={{ className: "items-center justify-between px-2" }}>
-                                    <p>{t('Calendar.VisitsCount')}:</p>
-                                    <p>{visitsPatients.current.visits.length}</p>
-                                </Stack>
-
-                                <div className={`flex-grow`}>
-                                    <Stack stackRef={cardRef} direction="vertical" stackProps={{ className: 'rounded-md border size-full p-4' }}>
-                                        {visitsPatients.current.visits.map((v, i) =>
-                                            <Stack key={i} stackProps={{ className: 'items-center w-full justify-between' }}>
-                                                <div className="text-nowrap overflow-auto text-center" style={{ width: cardWidth.current ? `${cardWidth.current / 3}px` : undefined }}>{visitsPatients.current.patients.find(p => p._id === v.patientId)?.firstName}</div>
-                                                <Separator orientation="vertical" />
-                                                <div className="text-nowrap overflow-auto text-center" style={{ width: cardWidth.current ? `${cardWidth.current / 3}px` : undefined }}>{visitsPatients.current.patients.find(p => p._id === v.patientId)?.lastName}</div>
-                                                <Separator orientation="vertical" />
-                                                <div className="text-nowrap overflow-auto text-center" style={{ width: cardWidth.current ? `${cardWidth.current / 3}px` : undefined }}>{typeof v.due === 'number' ? toFormat(v.due as number, configuration.local, undefined, DATE) : '-'}</div>
-                                            </Stack>
-                                        )}
+                            {animationFinish.current && !fetchingVisits.current && showVisitsStats.current &&
+                                <Stack direction='vertical' stackProps={{ className: `h-full ${animationFinish.current && !fetchingVisits.current && showVisitsStats.current ? '' : ''}` }}>
+                                    <Stack stackProps={{ className: "items-center justify-between px-2" }}>
+                                        <p>{t('Calendar.PatientsCount')}:</p>
+                                        <p>{patientsCount.current}</p>
                                     </Stack>
-                                </div>
-                            </Stack>
+                                    <Stack stackProps={{ className: "items-center justify-between px-2" }}>
+                                        <p>{t('Calendar.VisitsCount')}:</p>
+                                        <p>{visitsPatients.current.visits.length}</p>
+                                    </Stack>
+
+                                    <div className={`flex-grow`}>
+                                        <Stack stackRef={cardRef} direction="vertical" stackProps={{ className: 'rounded-md border size-full p-4' }}>
+                                            {visitsPatients.current.visits.map((v, i) =>
+                                                <Stack key={i} stackProps={{ className: 'items-center w-full justify-between' }}>
+                                                    <div className="text-nowrap overflow-auto text-center" style={{ width: cardWidth.current ? `${cardWidth.current / 3}px` : undefined }}>{visitsPatients.current.patients.find(p => p._id === v.patientId)?.firstName}</div>
+                                                    <Separator orientation="vertical" />
+                                                    <div className="text-nowrap overflow-auto text-center" style={{ width: cardWidth.current ? `${cardWidth.current / 3}px` : undefined }}>{visitsPatients.current.patients.find(p => p._id === v.patientId)?.lastName}</div>
+                                                    <Separator orientation="vertical" />
+                                                    <div className="text-nowrap overflow-auto text-center" style={{ width: cardWidth.current ? `${cardWidth.current / 3}px` : undefined }}>{typeof v.due === 'number' ? toFormat(v.due as number, configuration.local, undefined, DATE) : '-'}</div>
+                                                </Stack>
+                                            )}
+                                        </Stack>
+                                    </div>
+                                </Stack>
+                            }
                         </div>
                     </div>
                 </div>
